b1a3eaadbacfa65808b60b56f06121c2dd92f0a2
Ryukio <florian.fejer@hotmail.de>
2020-07-03 17:23:11 +0200

    Updating and deleting mongodb

--- mongodb.js
+++ mongodb.js
@@ -12,11 +12,31 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     
     const db = client.db(databaseName);
 
-    db.collection('tasks').findOne({ _id: ObjectID("5efc3be82f876037cc8bce80") }, (error, task) => {
+    /* db.collection('tasks').findOne({ _id: ObjectID("5efc3be82f876037cc8bce80") }, (error, task) => {
         console.log(task);
     })
 
     db.collection('tasks').find({ completed: true }).toArray((error, tasks) => {
         console.log(tasks);
+    }) */
+
+    /* db.collection('tasks').updateMany({ 
+        completed: false 
+    }, {
+        $set: {
+            completed: true,
+        }
+    }).then((result) => {
+        console.log(result)
+    }).catch((error) => {
+        console.log(error);
+    }) */
+
+    db.collection('tasks').deleteOne({
+      _id: ObjectID("5efc3be82f876037cc8bce80")  
+    }).then((result) => {
+        console.log(result);
+    }).catch((error) => {
+        console.log(error);
     })
 });
